3b29f1b7a4791cf491dd616e0fb3d04afd830398
Mimmlit <152621354+Matti1110@users.noreply.github.com>
2024-01-30 13:54:41 +0000

    wowwowowoowwwwwwwwwwwwwwwwwwwwwwwwoooooow

--- images.g.ts
+++ images.g.ts
@@ -22,25 +22,6 @@ e e d d d f d d d f d d d e e .
 . a a . . a a . d . . . . . . . 
 f f f . . . f f . . . . . . . . 
 `;
-            case "image2":
-            case "myImage0":return img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . e e e e e e . . . . . 
-. . . . e e d d d d e e . . . . 
-. . . . e d d d d d d e . . . . 
-. . . . e d f d d f d e . . . . 
-. . . . e d d d d d d e . . . . 
-. . . . e e d d d d e e . . . . 
-. . . . . . . d d . . . . . . . 
-. . . . . 7 7 7 7 7 7 . . . . . 
-. . . . . 7 7 7 7 7 7 . . . . . 
-. . . . . 7 a a a a 7 . . . . . 
-. . . . . d a . . a d . . . . . 
-. . . . . f f . . f f . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`;
             case "image3":
             case "myImage1":return img`
 . . . . . . . . . . . . . . . . 
@@ -59,6 +40,25 @@ f f f . . . f f . . . . . . . .
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "myImage0":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f e e e e e e f . . . . 
+. . . f e e e e e e e e f . . . 
+. . . f e e f f f f e e f . . . 
+. . . f e f f d d f f 6 f . . . 
+. . f e f d f d d f d f e f . . 
+. . f e f d 3 d d 3 d f e f . . 
+. . f e e f d d d d f e e f . . 
+. f e e f 7 f f f f 7 f e e f . 
+. . f f d 7 7 7 7 7 7 d f f . . 
+. . f d d 7 7 7 7 7 7 d d f . . 
+. . . f f 7 7 7 7 7 7 f f . . . 
+. . . . f a a a a a a f . . . . 
+. . . . f a a f f a a f . . . . 
+. . . . f f f . . f f f . . . . 
 `;
         }
         return null;
